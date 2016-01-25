870010c9b150c44e7edc797edeae65959f378fb4
jiang <jh235@qq.com>
2016-01-25 18:02:11 +0800

    Initial Commit

---
+++ emotion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  emotion
+//
+//  Created by jianghong on 16/1/25.
+//  Copyright © 2016年 jianghong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
